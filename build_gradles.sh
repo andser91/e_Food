1e5b5bd4ccbe1dfd16b0b0102464d94125bcc5c9
Elena Bernardi <elenabernardi15@gmail.com>
2019-03-04 10:23:52 +0100

    aggiunto sh per gradle assemble aggiunte istruzioni scritto file init.sql

---
+++ build_gradles.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo 'Inizio dei build'
+gradle assemble
---
+++ init.sql
@@ -0,0 +1,6 @@
+#database dei customers
+CREATE DATABASE customers;
+#database dei restaurantsa
+CREATE DATABASE restaurants;
+#database degli ordini
+CREATE DATABASE orders;
