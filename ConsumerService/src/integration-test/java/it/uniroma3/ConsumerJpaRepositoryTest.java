fe06d19fd9fef04b8b380998e2f6d1adeec1ccd0
Elena Bernardi <elenabernardi15@gmail.com>
2019-03-16 16:13:09 +0100

    aggiunti due nuovi test

--- ConsumerService/src/integration-test/java/it/uniroma3/ConsumerJpaRepositoryTest.java
+++ ConsumerService/src/integration-test/java/it/uniroma3/ConsumerJpaRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.support.TransactionTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,14 +24,16 @@ public class ConsumerJpaRepositoryTest {
     @Autowired
     private TransactionTemplate transactionTemplate;
 
-    private static final String CONSUMER_FIRST_NAME = "John";
-    private static final String CONSUMER_LAST_NAME = "Smith";
-    private static final Long CONSUMER_ID = 5L;
+    private static final String CONSUMER_FIRST_NAME_1 = "John";
+    private static final String CONSUMER_LAST_NAME_1 = "Smith";
+    private static final String CONSUMER_FIRST_NAME_2 = "Mary";
+    private static final String CONSUMER_LAST_NAME_2 = "Kate";
+
 
     @Test
     public void saveAndFindByIdConsumerTest(){
         Long consumerId = transactionTemplate.execute((ts) -> {
-           Consumer consumer = new Consumer(CONSUMER_FIRST_NAME, CONSUMER_LAST_NAME);
+           Consumer consumer = new Consumer(CONSUMER_FIRST_NAME_1, CONSUMER_LAST_NAME_1);
            consumerRepository.save(consumer);
            return consumer.getId();
         });
@@ -38,8 +42,8 @@ public class ConsumerJpaRepositoryTest {
         {
             Consumer consumer = consumerRepository.findById(consumerId).get();
 
-            assertThat(consumer.getLastName()).isEqualTo(CONSUMER_LAST_NAME);
-            assertThat(consumer.getFirstName()).isEqualTo(CONSUMER_FIRST_NAME);
+            assertThat(consumer.getLastName()).isEqualTo(CONSUMER_LAST_NAME_1);
+            assertThat(consumer.getFirstName()).isEqualTo(CONSUMER_FIRST_NAME_1);
             return null;
         });
     }
@@ -47,7 +51,7 @@ public class ConsumerJpaRepositoryTest {
     @Test
     public void saveAndDeleteByIdConsumerTest() {
         Long consumerId = transactionTemplate.execute((ts) -> {
-            Consumer consumer = new Consumer(CONSUMER_FIRST_NAME, CONSUMER_LAST_NAME);
+            Consumer consumer = new Consumer(CONSUMER_FIRST_NAME_1, CONSUMER_LAST_NAME_1);
             consumerRepository.save(consumer);
             return consumer.getId();
         });
@@ -61,5 +65,43 @@ public class ConsumerJpaRepositoryTest {
             assertThat(consumerNull).isEmpty();
             return null;
         } );
+    }
+
+    @Test
+    public void findAllConsumerTest(){
+        List<Consumer> consumers = transactionTemplate.execute((ts) -> {
+            Consumer c1 = new Consumer(CONSUMER_FIRST_NAME_1, CONSUMER_LAST_NAME_1);
+            Consumer c2 = new Consumer(CONSUMER_FIRST_NAME_2, CONSUMER_LAST_NAME_2);
+            List<Consumer> consumerList = new ArrayList<Consumer>();
+            consumerList.add(c1);
+            consumerList.add(c2);
+            consumerRepository.save(c1);
+            consumerRepository.save(c2);
+            return consumerList;
+        } );
+
+        transactionTemplate.execute((ts) -> {
+            List<Consumer> consumerList = (List<Consumer>) consumerRepository.findAll();
+            assertThat(consumerList.size()).isEqualTo(consumers.size());
+            assertThat(consumerList.get(0).getFirstName()).isEqualTo(consumers.get(0).getFirstName());
+            assertThat(consumerList.get(1).getFirstName()).isEqualTo(consumers.get(1).getFirstName());
+            assertThat(consumerList.get(0).getLastName()).isEqualTo(consumers.get(0).getLastName());
+            assertThat(consumerList.get(1).getLastName()).isEqualTo(consumers.get(1).getLastName());
+            return null;
+        });
+    }
+
+    @Test
+    public void findAllEmptyConsumerTest() {
+        transactionTemplate.execute((ts) -> {
+                    consumerRepository.deleteAll();
+                    return null;
+                });
+        transactionTemplate.execute((ts) -> {
+            List<Consumer> consumerList = (List<Consumer>) consumerRepository.findAll();
+            assertThat(consumerList.size()).isEqualTo(0);
+            assertThat(consumerList).isEmpty();
+            return null;
+        });
     }
 }
